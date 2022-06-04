bebc687afd96d7323852aff94243451bd5ff3af8
Fome-a <ofomearubs@gmail.com>
2022-06-04 21:12:16 +0000

    tasks

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-for i in range (0,99)
+for i in range(0, 100):
+    if i <= 9:
+        print("{:02d},".format(i), end=" ")
+    if i == 99:
+        print("{:02d}".format(i))
+    if i > 9:
+        print("{:02d},".format(i), end=" ")
