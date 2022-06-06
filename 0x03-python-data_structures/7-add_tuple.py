2bc88573a5f6d2c3ecf26e59f475719995c0e82a
Fome-a <ofomearubs@gmail.com>
2022-06-06 15:28:00 +0000

    tasks

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,7 +3,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
     new_a = len(tuple_a)
     new_b = len(tuple_b)
 
-
     if new_a == 0:
         a0 = 0
         a1 = 0
@@ -22,7 +21,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     else:
         b0 = tuple_b[0]
         b1 = tuple_b[1]
-    
+
     add1_tuple = a0 + b0
     add2_tuple = a1 + b1
 
