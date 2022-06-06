26c8c9e86855bfc80007c89c7fe49472bc7b0c7e
Fome-a <ofomearubs@gmail.com>
2022-06-06 15:46:40 +0000

    tasks

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,8 +2,8 @@ def multiple_returns(sentence):
     length = len(sentence)
     first_ch = sentence[0]
 
-    if length == None:
+    if length is None:
         first_ch = None
 
-    tuple_a = (length, first_ch) 
-    return(tuple_a)   
+    tuple_a = (length, first_ch)
+    return(tuple_a)
