b560de0b6518d8e1c091740e7ff170713cbf5cfb
Fome-a <ofomearubs@gmail.com>
2022-06-08 22:36:15 +0100

    tasks

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,25 +1,24 @@
-#!/usr/bin/python
+import sys
 if __name__ == "__main__":
-    import sys
-
+    operator = sys.argv[2]
     length = len(sys.argv) - 1
     if length > 2:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    operator = sys.argv[2]
-    if operator != '+' and operator != '-' and operator != '*' and operator != '/':
+    elif operator != '+' and operator != '-' and operator != '*' and operator != '/':
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-
-    from calculator_1 import add, sub, mul, div
-    a = sys.argv[1]
-    b = sys.argv[3]
-    if operator == '+':
-        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    elif operator == '-':
-        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    elif operator == '*':
-        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
     else:
-        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+        from calculator_1 import add, sub, mul, div
+        a = sys.argv[1]
+        b = sys.argv[3]
+
+        if operator == '+':
+            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+        elif operator == '-':
+            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+        elif operator == '*':
+            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+        else:
+            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
