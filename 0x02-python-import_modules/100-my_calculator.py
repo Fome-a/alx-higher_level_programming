04fff6cc2c4dfaadf398733a243a99180219b6d7
Fome-a <ofomearubs@gmail.com>
2022-06-08 22:29:22 +0100

    tasks

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -11,15 +11,15 @@ if __name__ == "__main__":
     if operator != '+' and operator != '-' and operator != '*' and operator != '/':
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-    a = sys.argv[1]
-    b = sys.argv[3]
 
     from calculator_1 import add, sub, mul, div
+    a = sys.argv[1]
+    b = sys.argv[3]
     if operator == '+':
         print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    if operator == '-':
+    elif operator == '-':
         print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    if operator == '*':
+    elif operator == '*':
         print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-    if operator == '/':
+    elif operator == '/':
         print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
