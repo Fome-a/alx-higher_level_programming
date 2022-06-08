9373e8033d6b731fcd56bc0a45ba1fff13167f39
Fome-a <ofomearubs@gmail.com>
2022-06-08 10:05:15 +0100

    tasks

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,11 +4,7 @@ def multiple_returns(sentence):
     first_ch = sentence[0]
 
     if length is None:
-<<<<<<< HEAD
-        tuple_a = (length,None)
-=======
         tuple_a = (length, None)
->>>>>>> 9d409bbed6ee78e62c71afd41e933ffb7ef97a63
     else:
         tuple_a = (length, first_ch)
     return(tuple_a)
