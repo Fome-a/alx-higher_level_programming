adace2aeb2da58c7a19be22ef67197da2d80953b
Fome-a <ofomearubs@gmail.com>
2022-06-08 22:26:31 +0100

    tasks

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 if __name__ == "__main__":
     import sys
-    from calculator_1 import add, sub, mul, div
+
     length = len(sys.argv) - 1
     if length > 2:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
