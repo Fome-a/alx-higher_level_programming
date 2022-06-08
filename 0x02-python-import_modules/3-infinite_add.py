a664afb841309988b0aa431968b49f341c1c2154
Fome-a <ofomearubs@gmail.com>
2022-06-08 12:31:00 +0100

    tasks

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
     i = len(sys.argv) - 1
 
     if i == 0:
@@ -12,4 +9,3 @@ if __name__ == "__main__":
         print("{}".format(i))
     else:
         sum(sys.argv)
-    
