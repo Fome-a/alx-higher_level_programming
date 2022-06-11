b9013caf99ba5f5c69f7e7bbc34072cf2fd0ccb4
Fome-a <ofomearubs@gmail.com>
2022-06-11 15:55:35 +0100

    tasks

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,7 +3,7 @@ def multiply_by_2(a_dictionary):
     new_dir = a_dictionary.copy()
     list_1 = list(new_dir.keys())
 
-    for key in new_dir:
+    for key in list_1:
         new_dir[key] *= 2
 
     return (new_dir)
