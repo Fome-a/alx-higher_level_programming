87b4df10f22e36faa3c63214ee1cb35f29735f1c
Fome-a <ofomearubs@gmail.com>
2022-06-22 15:28:58 +0100

    tasks

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -19,9 +19,9 @@ class Square:
     def size(self, size):
         """Setter for the size private attribute"""
         if type(size) is not int:
-            raise(TypeError("size must be an integer"))
+            raise TypeError("size must be an integer")
         elif size < 0:
-            raise(ValueError("size must be >= 0"))
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
 
