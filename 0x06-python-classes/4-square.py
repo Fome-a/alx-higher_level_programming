c631164aaa862e923ff0b3c6b73d9e6bb69c126a
Fome-a <ofomearubs@gmail.com>
2022-07-17 22:55:03 +0100

    tasks

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -16,14 +16,14 @@ class Square:
         return self.__size
 
     @size.setter
-    def size(self, size):
+    def size(self,value):
         """Setter for the size private attribute"""
-        if type(size) is not int:
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = size
+            self.__size = value
 
     def area(self):
         """Public method returns the square of size"""
