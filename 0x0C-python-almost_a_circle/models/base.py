0d8c1b804023705cf40ffe4b1c15531b195c37de
Fome-a <ofomearubs@gmail.com>
2022-07-27 12:12:32 +0100

    tasks

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Creating a base class"""
+class Base:
+    """defining class attribute"""
+    __nb_objects = 0
+    
+    def __init__(self, id=None):
+        """Initializing class base """
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects +=1
+            self.id = Base.__nb_objects
+        
