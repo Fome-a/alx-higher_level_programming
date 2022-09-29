49c097ce4fc3bd98dd6be547b09d764a35fb1baf
Fome-a <ofomearubs@gmail.com>
2022-09-29 13:12:10 +0100

    tasks

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
             db=database,
             charset="utf8")
         cur = conn.cursor()
-        cur.execute("SELECT states.name, cities.name FROM states, cities ORDER BY id ASC")
+        cur.execute("SELECT states.name, cities.name FROM states, cities ORDER BY states_id ASC")
         query_rows=cur.fetchall()
         for row in query_rows:
             print(row) 
